bde23761a2ea3d1d1134d628aea3b89774f0ff06
choibumsu <45811351+choibumsu@users.noreply.github.com>
2019-01-07 19:47:55 +0900

    Update update_process.php

--- update_process.php
+++ update_process.php
@@ -1,5 +1,29 @@
 <?php
-file_put_contents("data/".$_POST['old_title'], $_POST['description']);
-rename('data/'.$_POST['old_title'], 'data/'.$_POST['title']);
-header('Location: /index.php?id='.$_POST['title']);
+$conn=mysqli_connect("localhost", "root", "111111", "bumsu");
+
+settype($_POST['id'], 'integer');
+$filtered = array(
+  'id' => mysqli_real_escape_string($conn, $_POST['id']),
+  'title' => mysqli_real_escape_string($conn, $_POST['title']),
+  'description' => mysqli_real_escape_string($conn, $_POST['description'])
+);
+
+$sql = "
+  UPDATE topic
+    SET
+      title = '{$filtered['title']}',
+      description = '{$filtered['description']}'
+    WHERE id = {$filtered['id']}
+  ";
+$result = mysqli_query($conn, $sql);
+if($result === false) {
+  echo 'error';
+  error_log(mysqli_error($conn));
+}
+else {
+  $sql = "SELECT * FROM topic WHERE id={$filtered['id']}";
+  $res = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($res);
+  header('Location: /index.php?id='.$row['id']);
+}
 ?>
