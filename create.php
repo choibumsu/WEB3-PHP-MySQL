1d580c32f2fd0154e2e793777a74c3dc3edf6ab7
choibumsu <45811351+choibumsu@users.noreply.github.com>
2019-01-07 23:25:01 +0900

    Update create.php

--- create.php
+++ create.php
@@ -5,6 +5,7 @@ require_once ('view/top.php');
   <form action="create_process.php" method="post">
     <p><input type="text" name="title" placeholder="title"></p>
     <p><textarea name="description" placeholder="description"></textarea></p>
+    <p><?php author_list(); ?><p>
     <p><input type="submit"></p>
   </form>
 <?php
