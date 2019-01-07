18eae2620f00ad3f9a5c31c4202af0b054eb943e
choibumsu <45811351+choibumsu@users.noreply.github.com>
2019-01-07 19:46:08 +0900

    Update delete_process.php

--- delete_process.php
+++ delete_process.php
@@ -1,4 +1,19 @@
 <?php
-unlink("data/".$_POST['id']);
-header('Location: /index.php');
+$conn=mysqli_connect("localhost", "root", "111111", "bumsu");
+
+settype($_POST['id'], 'integer');
+$filtered_id = mysqli_real_escape_string($conn, $_POST['id']);
+
+$sql = "
+  DELETE FROM topic
+    WHERE id = {$filtered_id}
+  ";
+
+$result = mysqli_query($conn, $sql);
+if($result === false) {
+  echo 'error';
+  error_log(mysqli_error($conn));
+}
+else
+  header('Location: /index.php');
 ?>
