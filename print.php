6023eff8e82c7bf35c6f9fa79f8e18709bc737c8
choibumsu <45811351+choibumsu@users.noreply.github.com>
2019-01-07 23:25:50 +0900

    Update print.php

--- print.php
+++ print.php
@@ -2,13 +2,10 @@
 function print_title() {
   if(isset($_GET['id'])) {
     $conn=mysqli_connect("localhost", "root", "111111", "bumsu");
-
     $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
-
     $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
     $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
-
     $escaped_title = htmlspecialchars($row['title']);
     echo $escaped_title;
   }
@@ -29,17 +26,40 @@ function print_list() {
 function print_desc() {
   if(isset($_GET['id'])) {
     $conn=mysqli_connect("localhost", "root", "111111", "bumsu");
-
     $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
-
     $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
     $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
-
     $escaped_desc = htmlspecialchars($row['description']);
     echo $escaped_desc;
   }
   else
     echo "This is a <u>Bumsu's Home</u>";
 }
+
+function print_name() {
+  if(isset($_GET['id'])) {
+    $conn=mysqli_connect("localhost", "root", "111111", "bumsu");
+    $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
+    $sql = "SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id={$filtered_id}";
+    $res = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($res);
+    $escaped_name = htmlspecialchars($row['name']);
+    $escaped_name = "by ".$escaped_name;
+    echo $escaped_name;
+  }
+}
+
+function author_list() {
+    $conn=mysqli_connect("localhost", "root", "111111", "bumsu");
+    $sql = "SELECT * FROM author";
+    $res = mysqli_query($conn, $sql);
+    $select_author='<select name="author_id">';
+    while($row=mysqli_fetch_array($res))
+      $select_author.= '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+    $select_author.='</select>';
+
+    echo $select_author;
+}
 ?>
+
