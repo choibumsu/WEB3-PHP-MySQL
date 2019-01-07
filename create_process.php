c09efee0d7d6985858e2dad87275c285b53a3d13
choibumsu <45811351+choibumsu@users.noreply.github.com>
2019-01-07 23:25:23 +0900

    Update create_process.php

--- create_process.php
+++ create_process.php
@@ -1,20 +1,21 @@
 <?php
 $conn=mysqli_connect("localhost", "root", "111111", "bumsu");
-
 $filtered = array(
   'title' => mysqli_real_escape_string($conn, $_POST['title']),
-  'description' => mysqli_real_escape_string($conn, $_POST['description'])
+  'description' => mysqli_real_escape_string($conn, $_POST['description']),
+  'author_id' => mysqli_real_escape_string($conn, $_POST['author_id'])
 );
-
 $sql = "
   INSERT INTO topic
-    (title, description, created)
+    (title, description, created, author_id)
     VALUES(
       '{$filtered['title']}',
       '{$filtered['description']}',
-      NOW()
+      NOW(),
+      {$filtered['author_id']}
       )
   ";
+
 $result = mysqli_query($conn, $sql);
 if($result === false) {
   echo 'error';
